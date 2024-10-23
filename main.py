07f2db3d96b266737f764ccfe403b07f20178df1
Andrew <282hongshin@gmail.com>
2024-10-23 13:22:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Put your function here
-
+def timesTable(n):
+    timetable = []
+    for i in range (1, n+1):
+        row = []
+        for j in range(1, n+1):
+            row.append(i*j)
+        timetable.append(row)
+    return timetable
 
 # testing
 nums = int(input())
